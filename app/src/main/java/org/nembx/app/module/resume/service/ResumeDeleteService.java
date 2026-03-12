90cf2755679c39a175fec32c307996c7ac395dd7
nembx <3023881694@qq.com>
2026-03-12 21:48:36 +0800

    feat: 新增ResumeDelete服务

---
+++ app/src/main/java/org/nembx/app/module/resume/service/ResumeDeleteService.java
@@ -0,0 +1,35 @@
+package org.nembx.app.module.resume.service;
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.nembx.app.module.resume.enity.Resume;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * @author Lian
+ */
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class ResumeDeleteService {
+    private final ResumeManageService resumeManageService;
+
+    private final ResumeFileService resumeFileService;
+
+    @Transactional(rollbackFor = Exception.class)
+    public void deleteResume(Long resumeId) {
+        Resume resume = resumeManageService.getOneById(resumeId);
+        if (resume == null) {
+            log.warn("删除简历失败, id为: {}", resumeId);
+            return;
+        }
+        resumeManageService.deleteResume(resumeId);
+        log.info("数据库删除简历成功, id为: {}", resumeId);
+
+        resumeFileService.deleteResume(resume.getStorageKey());
+        log.info("文件系统删除简历成功, id为: {}", resumeId);
+    }
+}
