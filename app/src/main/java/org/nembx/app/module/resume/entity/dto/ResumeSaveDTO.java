0136dc8b18a544304f8e09b6f922322472868da0
nembx <3023881694@qq.com>
2026-03-20 22:04:50 +0800

    feat: Resume代码优化更新

--- app/src/main/java/org/nembx/app/module/resume/entity/dto/ResumeSaveDTO.java
+++ app/src/main/java/org/nembx/app/module/resume/entity/dto/ResumeSaveDTO.java
@@ -1,12 +1,12 @@
-package org.nembx.app.module.resume.enity.dto;
+package org.nembx.app.module.resume.entity.dto;
 
 
 /**
  * @author Lian
  */
 public record ResumeSaveDTO(
-        String fileKey,
-        String fileUrl,
+        String storageKey,
+        String storageUrl,
         String content,
         String fileHash,
         String fileName,
--- app/src/main/java/org/nembx/app/module/resume/service/ResumeUploadService.java
+++ app/src/main/java/org/nembx/app/module/resume/service/ResumeUploadService.java
@@ -4,19 +4,21 @@ package org.nembx.app.module.resume.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.nembx.app.common.exception.BusinessException;
-import org.nembx.app.common.exception.ErrorCode;
 import org.nembx.app.common.service.DocumentParseService;
 import org.nembx.app.common.service.FileCheckService;
-import org.nembx.app.module.resume.enity.Resume;
-import org.nembx.app.module.resume.enity.record.dto.ResumeDTO;
-import org.nembx.app.module.resume.enity.record.dto.ResumeSaveDTO;
-import org.nembx.app.module.resume.repository.ResumeAnalysisRepository;
-import org.nembx.app.module.resume.utils.FileHashUtils;
+import org.nembx.app.common.utils.FileHashUtils;
+import org.nembx.app.module.resume.entity.Resume;
+import org.nembx.app.module.resume.entity.dto.ResumeListenerDTO;
+import org.nembx.app.module.resume.entity.dto.ResumeSaveDTO;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+
 import java.util.Optional;
+
+import static org.nembx.app.common.exception.ErrorCode.BAD_REQUEST;
+import static org.nembx.app.common.exception.ErrorCode.UPLOAD_FAIL;
 
 /**
  * @author Lian
@@ -33,56 +35,50 @@ public class ResumeUploadService {
 
     private final FileCheckService fileCheckService;
 
-    private final ResumeAnalysisRepository resumeAnalysisRepository;
-
     private final ApplicationEventPublisher eventPublisher;
 
-    @Transactional(rollbackFor = BusinessException.class)
-    public void uploadAndAnalyze(MultipartFile file){
+    @Transactional(rollbackFor = Exception.class)
+    public void uploadAndAnalyze(MultipartFile file) {
         String contentType = file.getContentType();
         Long size = file.getSize();
         String originalFilename = file.getOriginalFilename();
         // 检查文件
         boolean valid = fileCheckService.isRealValidResume(file);
-        if (!valid){
+        if (!valid) {
             String fileExtension = fileCheckService.getFileExtension(originalFilename);
             log.error("不支持格式: {}, 请上传正确的简历文件", fileExtension);
-            throw new BusinessException(ErrorCode.BAD_REQUEST, "请上传正确的简历文件");
+            throw new BusinessException(UPLOAD_FAIL, "请上传正确的简历文件");
         }
         log.info("收到简历, 文件名为: {}, 大小为: {}", originalFilename, size);
 
-        Optional<Resume> existingResume = resumeManageService.checkIfDuplicate(file);
+
+        String fileHash = FileHashUtils.calculateHash(file);
+        Optional<Resume> existingResume = resumeManageService.checkIfDuplicate(fileHash);
         if (existingResume.isPresent()) {
-            String aiRes = handleDuplicateResume(existingResume.get());
-            log.info("重复简历最近的分析结果为: {}", aiRes);
+            log.info("检测到重复简历, resumeId={}", existingResume.get().getId());
+            throw new BusinessException(UPLOAD_FAIL, "该简历已上传过，请勿重复上传");
         }
 
         String content = documentParseService.parseContent(file);
-        if (content == null || content.isEmpty()){
+        if (content == null || content.isEmpty()) {
             log.warn("简历解析失败, 文件名为: {}", originalFilename);
-            throw new BusinessException(ErrorCode.BAD_REQUEST, "简历解析失败");
+            throw new BusinessException(BAD_REQUEST, "简历解析失败");
         }
-        log.info("简历解析成功, 文件名为: {}, 内容为: {}", originalFilename, content);
+        log.info("简历解析成功, 文件名为: {}, 内容长度: {} 字符", originalFilename, content.length());
+
+        // 保存简历
+        Long resumeId = resumeManageService.saveResume(
+                new ResumeSaveDTO(null, null, content,
+                        fileHash, originalFilename, size, contentType)
+        );
 
         String fileKey = resumeFileService.uploadResume(file);
         String fileUrl = resumeFileService.getResumeUrl(fileKey);
+        resumeManageService.updateResumeStorge(resumeId, fileKey, fileUrl);
         log.info("简历上传成功, 文件名为: {}, 文件路径为: {}", originalFilename, fileUrl);
-        // 保存简历
-        Long resumeId = resumeManageService.saveResume(
-                new ResumeSaveDTO(fileKey, fileUrl, content,
-                        FileHashUtils.calculateHash(file),
-                        originalFilename, size, contentType)
-        );
 
-        eventPublisher.publishEvent(new ResumeDTO(resumeId, content));
+
+        eventPublisher.publishEvent(new ResumeListenerDTO(resumeId, content));
         log.info("简历事件发布成功, 文件名为: {}", originalFilename);
-    }
-
-
-    private String handleDuplicateResume(Resume resume){
-        Long id = resume.getId();
-        log.info("检测到重复简历，返回最近历史分析结果: resumeId={}", id);
-        // TODO: 获取历史分析结果
-        return resumeAnalysisRepository.findFirstByResumeIdOrderByAnalyzedAtDesc(id).toString();
     }
 }
