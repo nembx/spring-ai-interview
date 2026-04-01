8fcdc50951ad256f3f1713ae81fbfedda0079c7d
nembx <3023881694@qq.com>
2026-04-01 23:43:19 +0800

    feat: 新增支持重新分析失败的简历

--- app/src/main/java/org/nembx/app/module/resume/controller/ResumeController.java
+++ app/src/main/java/org/nembx/app/module/resume/controller/ResumeController.java
@@ -5,7 +5,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import lombok.val;
 import org.nembx.app.common.exception.BusinessException;
 import org.nembx.app.common.result.Result;
 import org.nembx.app.module.resume.entity.pojo.Resume;
